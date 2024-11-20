fe73dc04d8dac4a8628505df12a64e30b664147a
Ahsan Ali <ahsanbutt6165@gmail.com>
2024-11-20 19:37:55 +0500

    jkdh

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import connectToMongo from "./database/db.js";
 import CardRoutes from "./routes/CardRoutes.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -18,7 +18,14 @@ connectToMongo();
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE", // Adjust as needed
+    allowedHeaders: "Content-Type,Authorization", // Add more headers if needed
+    credentials: true, // If you are using cookies, tokens, etc.
+  })
+);
 const PORT = process.env.PORT || 3000;
 
 // Routes
@@ -26,12 +33,6 @@ app.use("/api/user", UserRoutes);
 app.use("/api/board", boardRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/card", CardRoutes);
-
-// const __dirname = path.resolve("/Users/hp/Desktop/New folder");
-// app.use(express.static(path.join(__dirname, "/vite-project/dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "vite-project", "dist", "index.html"));
-// });
 
 // Start the server
 app.listen(PORT, () => {
